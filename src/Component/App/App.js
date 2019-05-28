54fe794303b2d94d45731e96372e6efce68c50b2
Konstantin <konstantin.v.modin@gmail.com>
2019-05-28 06:15:31 +0200

    update

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -10,45 +10,45 @@ function App() {
         {id: 223, name: 'Jhon', age: 32},
         {id: 224, name: 'Garold', age: 58},        
         {id: 226, name: 'Federique', age: 39},
-    ]);    
+    ]);
+    const [toggleBoll, setToggleBool] = useState(false);
 
     const inputHandler = ({ target }) => {
         setMyState(target.value);
         console.log(target, myState);
     }
 
-    const deleteHandler = ({target}) => {console.log(target)}; 
+    const deleteHandler = (id) => {    
+        const personIndex = personsList.findIndex(person => person.id === id);
+        setPersonsList([...personsList.slice(0, personIndex), ...personsList.slice(personIndex + 1)]);       
+    }; 
+
     const handleChange = (event, id) => {
         const personIndex = personsList.findIndex(person => person.id === id);
-        console.log(personIndex);
-
-        const newPerson = {
-            ...personsList[personIndex]
-        };
-        newPerson.name = event.target.value;
-
-        
-
-        setPersonsList([...personsList.slice(0, personIndex), newPerson, ...personsList.slice(personIndex + 1)]);
-        
+        const newPerson = {...personsList[personIndex], name: event.target.value};
+        setPersonsList([...personsList.slice(0, personIndex), newPerson, 
+            ...personsList.slice(personIndex + 1)]);        
     };
+
+    const showMeHandler = () => setToggleBool(!toggleBoll);
 
     return (
         <div className="App">
-            <h1>Hooks + Events</h1>
+            <h1>Hooks + Events</h1>            
             <UserInput defaultValue={myState} onChange={inputHandler}/>
             <UserOutput output={myState} />
-            {personsList.map(person => {
+            <button onClick={showMeHandler}>Show me!</button>
+            {toggleBoll ? personsList.map(person => {
                 return (            
                     <PersonItem 
                         key={person.id} 
                         name={person.name} 
                         age={person.age} 
-                        click={deleteHandler}
+                        click={() => deleteHandler(person.id)}
                         change={(event) => handleChange(event, person.id)}
                     />
                 );
-            })}
+            }) : null}
         </div>
     );
 }
