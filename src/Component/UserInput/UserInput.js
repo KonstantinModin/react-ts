f1359c315f6684f12baae38211ed34e4e47875d6
Konstantin <konstantin.v.modin@gmail.com>
2019-06-04 06:48:04 +0200

    updates

--- src/Component/UserInput/UserInput.js
+++ src/Component/UserInput/UserInput.js
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react';
 import classes from './UserInput.css';
 
 const UserInput = (props) => {
+    
+    
     useEffect(() => {
         console.log('useEfect! setup');
+        console.log(inputRef);
+        console.log(inputRef.current);
+        inputRef.current.focus();
         return () => {
             console.log('cleaning');
 
         }
-    }, []);
+    }, []);    
+    
+    const inputRef = React.createRef();
 
     return (
         <div className={classes.UserInput}>
             <h1>This is User Input</h1>
-            <input defaultValue={props.defaultValue} onChange={props.onChange}/>            
+            <input 
+                defaultValue={props.defaultValue} 
+                onChange={props.onChange}
+                ref={inputRef}
+            />            
         </div>
     )
 }
