a42ca44bb3a7054c7a74ce589b4e1e132fe735f9
Konstantin <konstantin.v.modin@gmail.com>
2019-06-04 06:30:06 +0200

    updates

--- src/Component/PersonList/PersonList.js
+++ src/Component/PersonList/PersonList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PersonItem from '../PersonItem';
 import ErrorBoundary from '../ErrorBoundary';
+import PropTypes from 'prop-types';
 
 const PersonList = (props) => props.list.map(person => {
         return (            
@@ -15,4 +16,10 @@ const PersonList = (props) => props.list.map(person => {
             
         );
     })
+
+PersonList.propTypes = {
+    list: PropTypes.array,
+    delete: PropTypes.func,
+    change: PropTypes.func
+}
 export default PersonList;
