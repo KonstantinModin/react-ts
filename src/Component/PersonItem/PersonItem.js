1ed1b37a6ebb28b19bedc880707cb6d61f9bd268
Konstantin <konstantin.v.modin@gmail.com>
2019-06-05 06:22:28 +0200

    updates

--- src/Component/PersonItem/PersonItem.js
+++ src/Component/PersonItem/PersonItem.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import classes from './PersonItem.css';
 import AuthContext from '../../context/authContext';
 
-export default class PersonItem extends Component {
-    state = {
-        hasError: false
-    }   
-
-    render() {
-        if (this.state.hasError) {
-            this.foo.bar = 0;
-        }
-
-        return (
-            <div className={classes.PersonItem}>                                  
-                <AuthContext.Consumer>
-                    {(context) =>  context ? 
-                        <><h3>{this.props.name}</h3>
-                            <p>That guy has an age of {this.props.age}</p>
-                            <input defaultValue={this.props.name} onChange={this.props.change} />
-                            <button onClick={this.props.click}>Delete me!</button>
-                            <button onClick={() => this.setState({hasError: true})}>Error</button></>
-                    : <h2>You should Log in First!</h2> }
-                </AuthContext.Consumer>                            
-            </div>
-        )
+const PersonItem = (props) => {
+    const [hasError, setHasError] = useState(false);
+    const myContext = useContext(AuthContext);
+    
+    if (hasError) {
+        this.foo = 0;
     }
+
+    return (
+        <div className={classes.PersonItem}>
+            {myContext ? 
+                <><h3>{props.name}</h3>
+                    <p>That guy has an age of {props.age}</p>
+                    <input defaultValue={props.name} onChange={props.change} />
+                    <button onClick={props.click}>Delete me!</button>
+                    <button onClick={() => setHasError(!hasError)}>Error</button></>
+            : <h2>You should Log in First!</h2> }                                     
+        </div>
+    )
+    
 }
+export default PersonItem
